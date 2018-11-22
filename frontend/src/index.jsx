066b4698113d07b76cdf79eff25a197f6e581735
serserch <serserch@yandex.ru>
2018-11-22 17:06:06 +0200

    Add fonts module

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,31 +1,38 @@
 /********************************************************************
-* Includes															*
-*********************************************************************/
+ * Includes															*
+ *********************************************************************/
 //Migration to typography v2
 window.__MUI_USE_NEXT_TYPOGRAPHY_VARIANTS__ = true;
+import WebFont from 'webfontloader';
+
+WebFont.load({
+    google: {
+        families: [
+            'Overpass:600',
+            'Source Sans Pro:400,700',
+            'sans-serif'
+        ]
+    }
+});
+
 /*Include styles*/
 import './sass/main.scss';
 
-/*Incude libraries*/ 
+/*Incude libraries*/
 import  React, {Component, Fragment} from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-//import { createStore, applyMiddleware} from 'redux';
-//import thunk from 'redux-thunk';
 
-/*Import user components*/
 import Navtop from 'components/Navtop';
 import Footer from 'components/Footer';
 
-//import 'react-holder-component';
-//import rootReduser from './reducers';
 import routes from './routes';
 import { store } from './store';
 
 /********************************************************************
-* Main															*
-*********************************************************************/
+ * Main															*
+ *********************************************************************/
 
 class App extends Component{
     render(){
@@ -44,7 +51,7 @@ class App extends Component{
         );
     }
 }
-/*Запуск отрисовки*/
+
 ReactDom.render(
     <Provider store = {store}>
         <BrowserRouter>
