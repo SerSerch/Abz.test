8c58c17365681e0ce71f144bde54f4a4ff03c45f
serserch <serserch@yandex.ru>
2018-11-27 21:35:29 +0200

    ES6 func

--- frontend/src/containers/UserLoginContainer.jsx
+++ frontend/src/containers/UserLoginContainer.jsx
@@ -3,21 +3,20 @@ import { connect } from 'react-redux';
 import { userSigningIn, userSigningOut, userSigningAuth } from 'actions/users';
 import Login from 'components/Login';
 
-
-function mapStateToProps(state, ownProps) {
+const mapStateToProps = (state, ownProps) => {
     return {
         ...ownProps,
         user: state.user,
     }
-}
+};
 
-function mapDispatchToProps(dispatch, props) {
+const mapDispatchToProps = (dispatch, props) => {
     return {
         ...props,
         userSigningIn: (data) => dispatch(userSigningIn(data)),
         userSigningOut: (data) => dispatch(userSigningOut(data)),
         userSigningAuth: (data) => dispatch(userSigningAuth(data)),
     }
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Login);
--- frontend/src/containers/UserLogupContainer.jsx
+++ frontend/src/containers/UserLogupContainer.jsx
@@ -4,20 +4,20 @@ import { userSigningUp, userSigningOut, userSigningAuth } from 'actions/users';
 import Logup from 'components/Logup';
 
 
-function mapStateToProps(state, ownProps) {
+const mapStateToProps = (state, ownProps) => {
     return {
         ...ownProps,
         user: state.user,
     }
-}
+};
 
-function mapDispatchToProps(dispatch, props) {
+const mapDispatchToProps = (dispatch, props) => {
     return {
         ...props,
         userSigningUp: (data) => dispatch(userSigningUp(data)),
         userSigningOut: (data) => dispatch(userSigningOut(data)),
         userSigningAuth: (data) => dispatch(userSigningAuth(data)),
     }
-}
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Logup);
