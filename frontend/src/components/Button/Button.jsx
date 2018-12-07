d167a9c3810d822fd4266224867360960b8f7cb7
SerSerch <serserch@yandex.ru>
2018-12-08 01:37:05 +0200

    Add button style in scss

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -2,49 +2,13 @@ import './Button.scss';
 
 import React, { PureComponent } from 'react';
 import ButtonM from "@material-ui/core/Button";
-import { withStyles } from '@material-ui/core/styles';
-
-const styles = theme => ({
-    contained: {
-        minWidth: '290px',
-        minHeight: '40px',
-        padding: '5px 15px',
-        backgroundColor: '#ef6c00',
-        '&:hover': {
-            backgroundColor: '#fc831f'
-        },
-    },
-    outlined: {
-        minWidth: '290px',
-        minHeight: '40px',
-        padding: '3px 13px',
-        border: '2px solid #ef6c00',
-        color: '#ef6c00',
-        '&:hover': {
-            padding: '3px 13px',
-            border: '2px solid #fc831f',
-            backgroundColor: '#fee6d2',
-            color: '#fc831f',
-        },
-    },
-    text: {
-        minWidth: '290px',
-        minHeight: '40px',
-        padding: '5px 15px',
-        '&:hover': {
-            padding: '5px 15px',
-            backgroundColor: 'transparent',
-            color: '#fc831f',
-        },
-    },
-});
 
 class Button extends PureComponent {
     render() {
-        const { variant, onClick, classes, text, ...other } = this.props;
+        const { variant, onClick, text, ...other } = this.props;
         return (
             <ButtonM
-                className={classes[variant]}
+                className={'button _' + variant}
                 variant={variant}
                 color="primary"
                 onClick={onClick}
@@ -56,4 +20,4 @@ class Button extends PureComponent {
     }
 }
 
-export default withStyles(styles)(Button);
+export default Button;
