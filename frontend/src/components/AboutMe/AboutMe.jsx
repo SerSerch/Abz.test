b235579ddf7befb77df54fced156ec41588791c8
SerSerch <serserch@yandex.ru>
2018-12-10 06:03:38 +0200

    Add About me style

--- frontend/src/components/AboutMe/AboutMe.jsx
+++ frontend/src/components/AboutMe/AboutMe.jsx
@@ -19,28 +19,26 @@ class AboutMe extends PureComponent {
 
         return (
             <Container className="aboutme" box>
-                <Item xs={12}>
+                <Item xs={12} className="aboutme__caption">
                     <Typography variant="h2" align="center">
-                        Let's get ac quainted
+                        Let's get acquainted
                     </Typography>
                 </Item>
                 <Item xs={12} md={4}>
-                    <img src={ManMobile} alt="Me"/>
+                    <img className="aboutme__man" src={ManMobile} alt="Me"/>
                 </Item>
                 <Item noSpace xs={12} md={8} container>
                     <Item>
                         <Typography variant="h3">
                             I am cool frontend developer
                         </Typography>
-                        <Typography>
+                        <Typography className="aboutme__text">
                             When real users have a slow experience on mobile, they're much less likely to find what they are looking for or purchase from you in the future. For many sites this equates to a huge missed opportunity, especially when more than half of visits are abandoned if a mobile page takes over 3 seconds to load.
                         </Typography>
-                        <Typography>
+                        <Typography className="aboutme__text">
                             Last week, Google Search and Ads teams announced two new speed initiatives to help improve user-experience on the web.
                         </Typography>
-                    </Item>
-                    <Item noSpace>
-                        <Button variant="text" text="Sign Up" onClick={SignUp} />
+                        <Button className="_aboutme" variant="text" text="Sign Up" onClick={SignUp} />
                     </Item>
                 </Item>
             </Container>
