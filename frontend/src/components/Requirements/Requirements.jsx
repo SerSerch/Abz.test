606f9ba93f5d63890a3f81196715b20e51d4d063
SerSerch <serserch@yandex.ru>
2018-12-10 20:54:24 +0200

    Add requirements style

--- frontend/src/components/Requirements/Requirements.jsx
+++ frontend/src/components/Requirements/Requirements.jsx
@@ -11,25 +11,26 @@ class Requirements extends PureComponent {
     render() {
         return (
             <div className="requirements">
-                <Container box>
-                    <Item xs={12}>
+                <Container box justify="center">
+                    <Item xs={12} md={8} lg={12} className="requirements__caption">
                         <Typography variant="h2" align="center" color="secondary">
                             General requirements for the test task
                         </Typography>
                     </Item>
-                    <Item lg={6}>
-                        <Typography color="secondary">
+                    <Item xs={12} md={6} className="requirements__text-box">
+                        <Typography className="requirements__text" color="secondary">
                             Users want to find answers to their questions quickly and data shows that people really care about how quickly their pages load. The Search team announced speed would be a ranking signal for desktop searches in 2010 and as of this month (July 2018), page speed will be a ranking factor for mobile searches too.
                         </Typography>
-                        <Typography color="secondary">
+                        <Typography className="requirements__text" color="secondary">
                             If you're a developer working on a site, now is a good time to evaluate your performance using our speed tools. Think about how performance affects the user experience of your pages and consider measuring a variety of real-world user-centric performance metrics.
                         </Typography>
-                        <Typography color="secondary">
+                        <Typography className="requirements__text" color="secondary">
                             Are you shipping too much JavaScript? Too many images? Images and JavaScript are the most significant contributors to the page weight that affect page load time based on data from HTTP Archive and the Chrome User Experience Report - our public dataset for key UX metrics as experienced by Chrome users under real-world conditions.
                         </Typography>
                     </Item>
-                    <Item lg={6}>
-                        <img src={ManLaptop1} alt=""/>
+                    <Item xs={12} md={6}>
+                        <img className="requirements__man section _mdnone" src={ManLaptop1} alt="Laptop"/>
+                        <img className="requirements__man section _md" src={ManLaptop2} alt="Laptop"/>
                     </Item>
                 </Container>
             </div>
