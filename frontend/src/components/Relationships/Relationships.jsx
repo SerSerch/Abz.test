728f6baa29f72d3a547ee4c6c28d8937f642a946
SerSerch <serserch@yandex.ru>
2018-12-10 06:04:17 +0200

    Add Relationships style

--- frontend/src/components/Relationships/Relationships.jsx
+++ frontend/src/components/Relationships/Relationships.jsx
@@ -12,43 +12,55 @@ import JsIcon from "svg/javascript.svg";
 class Relationships extends PureComponent {
     render() {
         return (
-            <Container className="aboutme" box justify="center">
-                <Item md={10} lg={6}>
+            <Container className="relationships" box justify="center">
+                <Item md={10} lg={8} xl={6}>
                     <Typography variant="h2" align="center">
                         About my relationships with
                         web-development
                     </Typography>
                 </Item>
-                <Item noSpace xs={12} container>
-                        <Item xs={12} lg={4}>
-                            <img src={HtmlIcon} alt="Me"/>
+                <Item noSpace xs={12} container justify="flex-end">
+                    <Item md={12} lg={4} container className="relationships__item">
+                        <Item xs={12} md={3} lg={12} noSpace>
+                            <img className="relationships__iamage" src={HtmlIcon} alt="Me"/>
+                        </Item>
+                        <Item xs={12} md={9} lg={12} noSpace className="relationships__about">
                             <Typography variant="h3">
                                 I'm in love with HTML
                             </Typography>
-                            <Typography>
+                            <Typography className="relationships__text">
                                 Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications.
                             </Typography>
                         </Item>
-                        <Item xs={12} lg={4}>
-                            <img src={CssIcon} alt="Me"/>
+                    </Item>
+                    <Item xs={12} md={12} lg={4} container className="relationships__item">
+                        <Item xs={12} md={3} lg={12} noSpace>
+                            <img className="relationships__iamage" src={CssIcon} alt="Me"/>
+                        </Item>
+                        <Item xs={12} md={9} lg={12} noSpace className="relationships__about">
                             <Typography variant="h3">
                                 CSS is my best friend
                             </Typography>
-                            <Typography>
+                            <Typography className="relationships__text">
                                 Cascading Style Sheets (CSS) is a  style sheet language used for describing the presentation
                                 of a document written in a markup language
                                 like HTML.
                             </Typography>
                         </Item>
-                        <Item xs={12} lg={4}>
-                            <img src={JsIcon} alt="Me"/>
+                    </Item>
+                    <Item xs={12} md={12} lg={4} container className="relationships__item">
+                        <Item xs={12} md={3} lg={12} noSpace>
+                            <img className="relationships__iamage" src={JsIcon} alt="Me"/>
+                        </Item>
+                        <Item xs={12} md={9} lg={12} noSpace className="relationships__about">
                             <Typography variant="h3">
                                 JavaScript is my passion
                             </Typography>
-                            <Typography>
+                            <Typography className="relationships__text">
                                 JavaScript is a high-level, interpreted programming language. It is a language which is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm.
                             </Typography>
                         </Item>
+                    </Item>
                 </Item>
             </Container>
         );
