7c1f97270bbeffa2bf5eeda1cb4614ba144aebd1
SerSerch <serserch@yandex.ru>
2018-12-10 06:04:32 +0200

    Delete lorem text

--- frontend/src/components/FirstDisplay/FirstDisplay.jsx
+++ frontend/src/components/FirstDisplay/FirstDisplay.jsx
@@ -29,7 +29,7 @@ class FirstDisplay extends PureComponent {
                                 We kindly remind you that your test assignment should
                                 be submitted as a link to github/bitbucket repository.
                                 Please be patient, we consider and respond to every application that meets minimum requirements.
-                                We look forward to your submission. Good luck! Lorem ipsum dolor sit amet, consectetur.
+                                We look forward to your submission. Good luck!
                             </Typography>
                         </Item>
                         <Item>
