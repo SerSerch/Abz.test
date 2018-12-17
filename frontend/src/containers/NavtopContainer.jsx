0d5ee467e1d6ed80f0e708b2bb46b6ac8e7e6b1e
SerSerch <serserch@yandex.ru>
2018-12-17 02:25:48 +0200

    Delete old actions

--- frontend/src/containers/NavtopContainer.jsx
+++ frontend/src/containers/NavtopContainer.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { /*userSigningIn, userSigningOut,*/ userSigningAuth, userGettingToken } from 'actions/users';
+import { userSigningAuth } from 'actions/users';
 import Navtop from 'components/Navtop';
 
 const mapStateToProps = (state, ownProps) => {
     return {
         ...ownProps,
-        user: state.user,
+        user: state.user.user,
+        error: state.user.error,
+        isLogined: state.user.isLogined,
+        userId: state.user.userId,
     }
 };
 
 const mapDispatchToProps = (dispatch, props) => {
     return {
         ...props,
-        //userSigningIn: (data) => dispatch(userSigningIn(data)),
-        //userSigningOut: (data) => dispatch(userSigningOut(data)),
         userSigningAuth: (data) => dispatch(userSigningAuth(data)),
-        userGettingToken: (data) => dispatch(userGettingToken(data)),
     }
 };
 
--- frontend/src/containers/SignUpContainer.jsx
+++ frontend/src/containers/SignUpContainer.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { userSigningUp } from 'actions/users';
+import { userSigningUp, userGettingToken, userGettingPosition } from 'actions/users';
 import SignUp from 'components/SignUp';
 
 
 function mapStateToProps(state, ownProps) {
     return {
         ...ownProps,
-        user: state.user,
+        user: state.user.user,
+        error: state.user.error,
+        token: state.user.token,
+        positions: state.user.positions,
     }
 }
 
@@ -15,6 +18,8 @@ function mapDispatchToProps(dispatch, props) {
     return {
         ...props,
         userSigningUp: (data) => dispatch(userSigningUp(data)),
+        userGettingToken: () => dispatch(userGettingToken()),
+        userGettingPosition: () => dispatch(userGettingPosition()),
     }
 }
 
